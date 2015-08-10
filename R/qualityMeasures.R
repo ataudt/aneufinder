91c1a2c9f1706cef859f4f1b6a22354bd0f2242f
chakalakka <a.s.taudt@umcg.nl>
2015-08-10 15:26:50 +0200

    preliminary quality measures

--- R/qualityMeasures.R
+++ R/qualityMeasures.R
@@ -7,6 +7,7 @@
 #' Spikyness is defined as \eqn{K = sum(abs(diff(reads))) / sum(reads)}.
 #' 
 #' @param reads A vector of binned read counts.
+#' @param hmm An \code{\link{aneuHMM}} object.
 #' @name qualityControl
 #' @author Aaron Taudt
 NULL
@@ -26,4 +27,44 @@ qc.entropy <- function(reads) {
 	n <- reads/total.reads
 	shannon.entropy <- -sum( n * log(n) , na.rm=TRUE)
 	return(shannon.entropy)
+}
+
+#' @describeIn qualityControl Calculate the Bhattacharyya distance between the 'monosomy' and 'disomy' distribution
+qc.bhattacharyya <- function(hmm) {
+	if (class(hmm)=='aneuHMM') {
+		distr <- hmm$distributions
+	} else if (class(hmm)=='aneuBiHMM') {
+		distr <- hmm$distributions$minus
+	}
+	x <- 0:max(max(hmm$bins$reads),500)
+	dist <- -log(sum(sqrt(dnbinom(x, size=distr['monosomy','size'], prob=distr['monosomy','prob']) * dnbinom(x, size=distr['disomy','size'], prob=distr['disomy','prob']))))
+	return(dist)
+}
+
+
+quality <- function(hmm) {
+
+	qframe <- data.frame(total.read.count=sum(hmm$bins$reads),
+												binsize=width(hmm$bins)[1],
+												avg.read.count=mean(hmm$bins$reads),
+												spikyness=hmm$qualityInfo$spikyness,
+												entropy=hmm$qualityInfo$shannon.entropy,
+												complexity=hmm$qualityInfo$complexity,
+												loglik=hmm$convergenceInfo$loglik,
+												num.segments=length(hmm$segments),
+												bhattacharyya=qc.bhattacharyya(hmm))
+	return(qframe)
+
+}
+												
+
+clusterByQuality <- function(hmms) {
+	
+	library(mclust)
+	hmms <- loadHmmsFromFiles(hmms)
+	df <- do.call(rbind, lapply(hmms, quality))
+	df <- df[c('spikyness','entropy','loglik','num.segments','bhattacharyya')]
+	fit <- Mclust(df)
+	return(fit)
+
 }
